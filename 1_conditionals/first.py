3e448a3f5328640c1c8f1a2e96187e2eeaa8b87c
Saket Khopkar <saketkhopkar910@gmail.com>
2025-09-03 14:49:48 +0530

    Create first.py

---
+++ 1_conditionals/first.py
@@ -0,0 +1,11 @@
+# You are creating a notificattion system for a smart kettle. When water boils, notify the user.
+# var -> kettle_boiled (boolean). If boiled, print "Water in kettle has boiled!".
+
+kettle_boiled = True
+
+if kettle_boiled:
+    print("Water in kettle has boiled!")
+
+# Expected output: "Water in kettle has boiled!" (if kettle_boiled is True)
+# If kettle_boiled is False, there should be no output.
+
