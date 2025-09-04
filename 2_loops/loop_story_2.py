d10e32dafd2eda6b7a76b0cf71909eb5bf18d1a4
Saket Khopkar <saketkhopkar910@gmail.com>
2025-09-04 10:49:54 +0530

    Create loop_story_2.py

---
+++ 2_loops/loop_story_2.py
@@ -0,0 +1,9 @@
+# We are to iterate through a list for this scenario.
+# The list contains the names of people invited to a party. The goal is to print a message for each person in the list.
+
+# Ex : Greetings, [name]. You are invited to the party! Lets Rock and Roll!
+
+invitees = ["Amol", "Rohan", "Sonal", "Soumya", "Sagar"]   # list
+
+for name in invitees:
+    print(f"Greetings, {name}. You are invited to my birthday party! Let's Rock and Roll!")
