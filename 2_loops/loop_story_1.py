6d49f530e00d34100e4612e360026d247497660c
Saket Khopkar <saketkhopkar910@gmail.com>
2025-09-04 10:21:46 +0530

    Update loop_story_1.py

--- 2_loops/loop_story_1.py
+++ 2_loops/loop_story_1.py
@@ -6,3 +6,11 @@
 
 for token_number in range(1, 11):      # Loop from 1 to 10; 11 is non-inclusive
     print(f"Customer #{token_number}, please collect your order")
+
+# Let's have another example
+# This cafe prepares orders in batch every 10 minutes; simulate 4 batches
+
+# TO DO : use range() to simulate batch numbers; and keep informing "Batch number i is ready"
+
+for batch in range(1, 5):
+    print(f"Batch number {batch} is ready.")
